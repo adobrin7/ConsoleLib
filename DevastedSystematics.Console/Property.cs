e10573e2399d9b6dac351f28bd6544acbf8e31ce
Alexandr Dobrin <dobrin.alexandrr@gmail.com>
2022-09-13 23:57:26 +0300

    Refactor readable property.

---
+++ DevastedSystematics.Console/Property.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+
+namespace DevastedSystematics.ConsoleLib;
+
+public sealed class Property
+{
+    internal Property(object containingInstance, PropertyInfo info)
+    {
+        this.containingInstance = containingInstance;
+        this.info = info;
+    }
+
+    private readonly object containingInstance;
+
+    private readonly PropertyInfo info;
+
+    public object? Value
+    {
+        get => info.GetValue(containingInstance); // todo remove?
+        set => info.SetValue(
+            containingInstance,
+            Convert.ChangeType(value, info.PropertyType));
+    }
+
+    public void SetDefaultValue() =>
+        info.SetValue(containingInstance, Activator.CreateInstance(info.PropertyType));
+}
---
+++ DevastedSystematics.Console/ReadableProperty.cs
@@ -0,0 +1,85 @@
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace DevastedSystematics.ConsoleLib;
+
+public sealed class ReadableProperty
+{
+    public ReadableProperty(object containingInstance, PropertyInfo info)
+    {
+        readableAttribute = info.GetCustomAttribute<ReadableAttribute>();
+        validAttribute = info.GetCustomAttribute<ValidatedOnReadingAttribute>();
+        isRequired = info.GetCustomAttribute<RequieredReadingAttribute>() is not null;
+        originalName = info.Name;
+        property = new Property(containingInstance, info);
+        CanRead = info.GetCustomAttribute<IgnoreReadingAttribute>() is null;
+    }
+
+    private readonly ReadableAttribute? readableAttribute;
+
+    private readonly ValidatedOnReadingAttribute? validAttribute;
+
+    private readonly bool isRequired;
+
+    private readonly string originalName;
+
+    private readonly Property property;
+
+    internal bool CanRead { get; }
+
+    public Message InvalidPatternMessage => new(
+        validAttribute?.ErrorMessage ?? "",
+        validAttribute?.MessageColor ?? ConsoleColor.Gray);
+
+    public object? Value => property.Value;
+
+    public bool StartReading(IPropertyNameFormatter? nameFormatter = null)
+    {
+        string nameToShow = GetFormattedName(nameFormatter);
+        string? input = ReadLine();
+        
+        if (IsReadingCanceled(input))
+            return true;
+        if(IsReadingSkipped(input))
+            return false;
+
+        if (!IsValidInput(input))
+        {
+            InvalidPatternMessage.Show();
+            Input.ReadUntilValid(validAttribute?.Pattern!, nameToShow, InvalidPatternMessage);
+        }
+
+        property.Value = input;
+
+        return false;
+    }
+
+    private string GetFormattedName(IPropertyNameFormatter? nameFormatter = null)
+    {
+        string nameToShow = readableAttribute?.Name ?? originalName;
+        return nameFormatter?.Format(nameToShow) ?? nameToShow;
+    }
+
+    private bool IsReadingCanceled(string? input) =>
+        readableAttribute?.CancelToken is not null &&
+        string.Equals(input, readableAttribute.CancelToken, StringComparison.OrdinalIgnoreCase);
+
+    private bool IsReadingSkipped(string? input) =>
+        readableAttribute?.SkipToken is not null &&
+        !isRequired &&
+        string.Equals(input, readableAttribute.SkipToken, StringComparison.OrdinalIgnoreCase);
+
+    private bool IsValidInput(string? input) =>
+        input is not null && 
+        (validAttribute?.Pattern is null || Regex.IsMatch(input, validAttribute.Pattern));
+
+    public void SetDefaultValue()
+    {
+        if (readableAttribute?.DefaultValue is null)
+        {
+            property.SetDefaultValue();
+            return;
+        }
+        property.Value = readableAttribute?.DefaultValue;
+    }
+}
